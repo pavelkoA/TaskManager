4a39bf61780b8bf12e1c6174375d4280c817a6c9
pavelkoA <pavelko88@mail.ru>
2024-07-25 18:57:59 +0300

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -102,14 +102,6 @@ DATABASES = {
 }
 
 
-if RENDER_EXTERNAL_HOSTNAME:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL'),
-            conn_max_age=600,
-        )
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
